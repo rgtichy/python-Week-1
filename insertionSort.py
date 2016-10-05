7be3ce87be04802285b7292ac88e8dd757805626
Robert Tichy <robtichy@AOL.com>
2016-10-05 07:00:01 -0400

    Wednesday AM commit selectionSort()

---
+++ insertionSort.py
@@ -0,0 +1,9 @@
+def insertionSort(list):
+    last=len(list)
+    front=0
+    demarc=0
+    for i in range(demarc+1,last):
+        if list[i]>list[demarc]:
+            demarc=i
+        else:
+            insert()
--- selectionSort2.py
+++ selectionSort2.py
@@ -4,15 +4,25 @@ def selectionSort(list):
     d = 1
     l = len(list)
     i = 0
-    for d in range(1,l):
+    for d in range(1,l/2):
         s = d - 1
+        t = l - 1
         for i in range(d-1,l):
             if (list[i]<list[s]):
                 s=i
+            if (list[i]>list[t]):
+                t=i
+        #put lowest found value at spot just before list[d]
         temp = list[d-1]
         list[d-1] = list[s]
         list[s] = temp
+        # put highest value at endpoint list[l]
+        temp = list[l-1]
+        list[l-1] = list[t]
+        list[t] = temp
+        # move lower bound up, upper bound down
         d = d + 1
+        l = l - 1
     return list
 sum = datetime.datetime.now()-datetime.datetime.now()
 print "start."
